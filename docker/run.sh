eccb558e91213e00d2dc57bb8fff6c8f88641f5b
Andrei Nesterov <ae.nesterov@gmail.com>
2017-11-05 19:24:48 +0300

    Simplify Docker files

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 source ./docker/default.profile
+DOCKER_CONTAINER_NAME="sandbox.build/${PROJECT}"
+DOCKER_CONTAINER_COMMAND=${DOCKER_CONTAINER_COMMAND:-'/bin/bash'}
+DOCKER_RUN_OPTIONS=${DOCKER_RUN_OPTIONS:-'-ti --rm'}
 
 read -r DOCKER_RUN_COMMAND <<-EOF
     cargo run
@@ -9,10 +12,10 @@ EOF
 set -xe
 docker build \
     --build-arg BUILD_DIR="${DOCKER_BUILD_DIR}" \
-    -t ${DOCKER_BUILDSTAGE_CONTAINER_NAME} \
+    -t ${DOCKER_CONTAINER_NAME} \
     -f ./docker/Dockerfile.build .
 docker run ${DOCKER_RUN_OPTIONS} \
     -p 8080:8080 \
     -v $(pwd)/src:${DOCKER_BUILD_DIR}/src \
-    ${DOCKER_BUILDSTAGE_CONTAINER_NAME} \
+    ${DOCKER_CONTAINER_NAME} \
     /bin/bash -c "set -x && cd ${DOCKER_BUILD_DIR} && ${DOCKER_RUN_COMMAND} && set +x && ${DOCKER_CONTAINER_COMMAND}"
